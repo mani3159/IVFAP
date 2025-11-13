e8df549cec2590f237c666f831c7379eb0d578ed
manikumar <mani96462@gmail.com>
2025-11-13 12:53:00 +0530

    eighth Commit

--- pptproject/settings.py
+++ pptproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -115,17 +115,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/history/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # if you have a static folder at project root
-]
 
-# Folder where collectstatic will collect static files (for production)
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # For dev, if using project static
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # For production/deployment
